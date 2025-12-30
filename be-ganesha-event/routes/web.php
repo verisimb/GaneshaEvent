2cc66958d03feb503425a183e1a5e778ba0ee94e
Very Simbolon <verisimb@gmail.com>
2025-12-30 09:42:47 +0800

    feat: tambah route /check-gd untuk verifikasi GD extension

--- be-ganesha-event/routes/web.php
+++ be-ganesha-event/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Debug route to check GD extension (temporary for Railway verification)
+Route::get('/check-gd', function () {
+    $extensions = get_loaded_extensions();
+    $hasGD = extension_loaded('gd');
+    
+    return response()->json([
+        'gd_installed' => $hasGD,
+        'gd_info' => $hasGD ? gd_info() : 'GD not installed',
+        'php_version' => phpversion(),
+        'all_extensions' => $extensions
+    ]);
+});
